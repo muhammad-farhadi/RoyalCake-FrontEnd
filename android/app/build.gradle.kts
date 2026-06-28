fa06d7093d8f61ce3ce1d60e3e5222901ea29468
Mohammad Farhadi <mohammad.farhadi.h@gmail.com>
2026-06-29 00:02:51 +0330

    ready to build

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,16 +7,16 @@ plugins {
 
 android {
     namespace = "ir.royalcakes.royalcakes"
-    compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    compileSdk = 36
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
+        jvmTarget = JavaVersion.VERSION_17.toString()
     }
 
     defaultConfig {
